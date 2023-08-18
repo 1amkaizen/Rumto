5b4669f6a9a558574a11f03f6a3631836ad50f2a
1amkaiz3n <99733941+1amkaizen@users.noreply.github.com>
2023-08-18 16:28:29 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,173 @@
+// script.js
+const inputDigits = document.querySelectorAll(".input-digit");
+const generateBtn = document.getElementById("generateBtn");
+const numberSequenceDisplay = document.getElementById("numberSequenceDisplay");
+const nonRedNumbers = document.getElementById("nonRedNumbers");
+const numberSequenceDisplaySubtract = document.getElementById(
+  "numberSequenceDisplaySubtract"
+);
+const generateBtnSubtract = document.getElementById("generateBtnSubtract");
+
+inputDigits.forEach((digitInput, index) => {
+  if (index < inputDigits.length - 1) {
+    digitInput.addEventListener("input", handleInputChange);
+  }
+});
+
+function handleInputChange(event) {
+  const input = event.target;
+
+  if (input.value === "") {
+    const prevInput = input.previousElementSibling;
+    if (prevInput) {
+      prevInput.focus();
+    }
+  }
+
+  if (input.value.length >= 1) {
+    const nextInput = input.nextElementSibling;
+    if (nextInput) {
+      nextInput.focus();
+    }
+  }
+}
+
+generateBtn.addEventListener("click", generateNumberSequence);
+generateBtnSubtract.addEventListener("click", generateNumberSequenceSubtract);
+
+function focusNextInput(event) {
+  const input = event.target;
+  if (input.value !== "") {
+    const nextInput = input.nextElementSibling;
+    if (nextInput) {
+      nextInput.focus();
+    }
+  }
+}
+
+function generateNumberSequence() {
+  const enteredNumber = Array.from(inputDigits)
+    .map((digitInput) => digitInput.value)
+    .join("");
+
+  if (enteredNumber.length !== 4 || isNaN(enteredNumber)) {
+    numberSequenceDisplay.innerHTML = "Please enter a valid 4-digit number.";
+    return;
+  }
+
+  numberSequenceDisplay.innerHTML = "";
+  nonRedNumbers.innerHTML = "";
+
+  const redNumbers = [];
+  const nonRed = [];
+  for (let i = 0; i < 5; i++) {
+    const numberLine = document.createElement("div");
+    numberLine.classList.add("number-line");
+    const shiftedNumber = shiftNumber(enteredNumber, i);
+
+    shiftedNumber.forEach((digit, index) => {
+      const digitBox = document.createElement("div");
+      digitBox.classList.add("number-box");
+      if ((i + index) % 2 === 0) {
+        digitBox.classList.add("red");
+        redNumbers.push(digit);
+      } else {
+        digitBox.classList.add("non-red");
+        if (nonRed.indexOf(digit) === -1) {
+          nonRed.push(digit);
+        }
+      }
+      digitBox.textContent = digit;
+      numberLine.appendChild(digitBox);
+    });
+
+    numberSequenceDisplay.appendChild(numberLine);
+  }
+
+  nonRed.forEach((digit) => {
+    const nonRedBox = document.createElement("div");
+    nonRedBox.classList.add("number-box", "non-red");
+    nonRedBox.textContent = digit;
+    nonRedNumbers.appendChild(nonRedBox);
+  });
+}
+function shiftNumber(number, shift, subtract) {
+  const shiftedNumber = [];
+  for (let i = 0; i < number.length; i++) {
+    const digit = parseInt(number[i]);
+    let shiftedDigit;
+    if (subtract) {
+      shiftedDigit = (digit - shift + 10) % 10;
+    } else {
+      shiftedDigit = (digit + shift) % 10;
+    }
+    shiftedNumber.push(shiftedDigit);
+  }
+
+  return shiftedNumber;
+}
+
+// ... kode sebelumnya ...
+
+function generateNumberSequenceSubtract() {
+  const enteredNumber = Array.from(inputDigits)
+    .map((digitInput) => digitInput.value)
+    .join("");
+
+  if (enteredNumber.length !== 4 || isNaN(enteredNumber)) {
+    numberSequenceDisplaySubtract.innerHTML =
+      "Please enter a valid 4-digit number.";
+    return;
+  }
+
+  // Clear previous results
+  numberSequenceDisplaySubtract.innerHTML = "";
+  nonRedNumbersSubtract.innerHTML = "";
+
+  const redNumbers = [];
+  const nonRedSubtract = [];
+
+  for (let i = 0; i < 5; i++) {
+    const numberLine = document.createElement("div");
+    numberLine.classList.add("number-line");
+    const shiftedNumber = shiftNumberSubtract(enteredNumber, i);
+
+    shiftedNumber.forEach((digit, index) => {
+      const digitBox = document.createElement("div");
+      digitBox.classList.add("number-box");
+      if ((i + index) % 2 === 0) {
+        digitBox.classList.add("red");
+        digitBox.textContent = digit; // Menampilkan angka di kotak merah
+        redNumbers.push(digit);
+      } else {
+        digitBox.classList.add("non-red");
+        digitBox.textContent = digit;
+        if (nonRedSubtract.indexOf(digit) === -1) {
+          nonRedSubtract.push(digit);
+        }
+      }
+      numberLine.appendChild(digitBox);
+    });
+
+    numberSequenceDisplaySubtract.appendChild(numberLine);
+  }
+
+  const nonRedNumbersContainerSubtract = document.createElement("div");
+  nonRedSubtract.forEach((digit) => {
+    const nonRedBox = document.createElement("div");
+    nonRedBox.classList.add("number-box", "non-red");
+    nonRedBox.textContent = digit;
+    nonRedNumbersContainerSubtract.appendChild(nonRedBox);
+  });
+  nonRedNumbersSubtract.appendChild(nonRedNumbersContainerSubtract);
+}
+
+function shiftNumberSubtract(number, shift) {
+  const shiftedNumber = [];
+  for (let i = 0; i < number.length; i++) {
+    const digit = parseInt(number[i]);
+    const shiftedDigit = (digit - shift + 10) % 10;
+    shiftedNumber.push(shiftedDigit);
+  }
+  return shiftedNumber;
+}
