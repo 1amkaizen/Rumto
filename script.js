03699a48d0675d01feb6e10b2fb5a18989611a16
1amkaiz3n <99733941+1amkaizen@users.noreply.github.com>
2023-09-02 11:59:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,29 +6,6 @@ const nonRedNumbers = document.getElementById("nonRedNumbers");
 const numberSequenceDisplaySubtract = document.getElementById(
   "numberSequenceDisplaySubtract"
 );
-const generateBtnSubtract = document.getElementById("generateBtnSubtract");
-
-generateBtn.addEventListener("click", () => {
-  // Tampilkan iklan di dalam #adsense-container
-  const adsenseContainer = document.getElementById("adsense-container");
-  adsenseContainer.style.display = "block";
-
-  // Tunggu hingga iklan selesai ditonton
-  adsbygoogle = window.adsbygoogle || [];
-  adsbygoogle.push(() => {
-    adsbygoogle.ads;
-    pauseAdRequests = true;
-    adsbygoogle.ads;
-    adsbygoogle.push({});
-  });
-
-  // Setelah iklan selesai, tampilkan hasil
-  adsenseContainer.addEventListener("adsbygoogle:adclosed", () => {
-    adsenseContainer.style.display = "none"; // Sembunyikan iklan
-    // Tampilkan hasil di sini
-    generateNumberSequence(); // Panggil fungsi yang menampilkan hasil
-  });
-});
 
 inputDigits.forEach((digitInput, index) => {
   if (index < inputDigits.length - 1) {
@@ -54,8 +31,17 @@ function handleInputChange(event) {
   }
 }
 
-generateBtn.addEventListener("click", generateNumberSequence);
-generateBtnSubtract.addEventListener("click", generateNumberSequenceSubtract);
+// Event listener untuk tombol "Rumus A"
+generateBtn.addEventListener("click", () => {
+  generateNumberSequence();
+  updateCombinedNonRedNumbers(); // Memanggil fungsi untuk memperbarui gabungan non-merah
+});
+
+// Event listener untuk tombol "Rumus B"
+generateBtnSubtract.addEventListener("click", () => {
+  generateNumberSequenceSubtract();
+  updateCombinedNonRedNumbers(); // Memanggil fungsi untuk memperbarui gabungan non-merah
+});
 
 function focusNextInput(event) {
   const input = event.target;
@@ -66,7 +52,12 @@ function focusNextInput(event) {
     }
   }
 }
+// Tambahkan variabel global untuk menyimpan angka-angka non-merah unik.
+const combinedNonRedNumbers = new Set();
+// Deklarasi variabel global
+let allNonRed = [];
 
+// ...
 function generateNumberSequence() {
   const enteredNumber = Array.from(inputDigits)
     .map((digitInput) => digitInput.value)
@@ -76,6 +67,10 @@ function generateNumberSequence() {
     numberSequenceDisplay.innerHTML = "Please enter a valid 4-digit number.";
     return;
   }
+
+  combinedNonRedNumbers.clear();
+  // Hapus konten sebelumnya di dalam randomNumbersContainer
+  randomNumbersContainer.innerHTML = "";
 
   numberSequenceDisplay.innerHTML = "";
   nonRedNumbers.innerHTML = "";
@@ -105,81 +100,36 @@ function generateNumberSequence() {
 
     numberSequenceDisplay.appendChild(numberLine);
   }
-
   nonRed.forEach((digit) => {
     const nonRedBox = document.createElement("div");
     nonRedBox.classList.add("number-box", "non-red");
     nonRedBox.textContent = digit;
     nonRedNumbers.appendChild(nonRedBox);
+
+    // Tambahkan angka non-merah ke dalam combinedNonRedNumbers
+    combinedNonRedNumbers.add(digit);
   });
 
-  // Setelah kode menampilkan hasil perhitungan, tambahkan ini
-  const randomNumberDisplay = document.getElementById("randomNumberDisplay");
-  randomNumberDisplay.innerHTML = ""; // Kosongkan konten sebelumnya
+  // ...
 
-  const randomButton = document.createElement("button");
-  randomButton.classList.add("btn", "btn-primary", "mt-3");
-  randomButton.textContent = "Generate";
-  randomButton.addEventListener("click", generateRandomNumberFromNonRed);
+  // Perbarui tampilan combinedNonRedNumbers
+  updateCombinedNonRedNumbers();
 
-  randomNumberDisplay.appendChild(randomButton);
-}
+  // ...
 
-function generateRandomNumberFromNonRed() {
-  const nonRedNumbers = getNonRedNumbers();
-  const nonRedNumbersSubtract = getNonRedNumbersSubtract();
-
-  const combinedNonRedNumbers = nonRedNumbers.concat(nonRedNumbersSubtract);
-
-  if (combinedNonRedNumbers.length < 4) {
-    return; // Hanya lanjutkan jika ada cukup angka non-merah dari kedua hasil perhitungan
-  }
-
-  const randomNumbers = Array.from({ length: 4 }, () => {
-    const randomIndex = Math.floor(
-      Math.random() * combinedNonRedNumbers.length
-    );
-    return combinedNonRedNumbers[randomIndex];
-  });
-
-  const randomNumberDisplay = document.getElementById("randomNumberDisplay");
-  const randomNumberParagraph = document.createElement("p");
-  randomNumberParagraph.textContent = `Hasil Prediksi: ${randomNumbers.join(
-    ""
-  )}`;
-
-  randomNumberDisplay.appendChild(randomNumberParagraph);
-}
-
-function getNonRedNumbers() {
-  const nonRedNumberBoxes = document.querySelectorAll(".number-box.non-red");
-  return Array.from(nonRedNumberBoxes, (box) => box.textContent);
-}
-
-function getNonRedNumbersSubtract() {
-  const nonRedNumberBoxesSubtract = document.querySelectorAll(
-    ".number-box.non-red"
+  // Tampilkan combinedNonRedNumbers tanpa duplikat dalam tampilan HTML.
+  const uniqueNonRedNumbers = Array.from(combinedNonRedNumbers);
+  const combinedNonRedNumbersContainer = document.getElementById(
+    "combinedNonRedNumbers"
   );
-  return Array.from(nonRedNumberBoxesSubtract, (box) => box.textContent);
+  combinedNonRedNumbersContainer.innerHTML = uniqueNonRedNumbers
+    .map((digit) => {
+      return `<div class="number-box non-red">${digit}</div>`;
+    })
+    .join("");
+  // Update variabel global allNonRed
+  allNonRed = nonRed;
 }
-function shiftNumber(number, shift, subtract) {
-  const shiftedNumber = [];
-  for (let i = 0; i < number.length; i++) {
-    const digit = parseInt(number[i]);
-    let shiftedDigit;
-    if (subtract) {
-      shiftedDigit = (digit - shift + 10) % 10;
-    } else {
-      shiftedDigit = (digit + shift) % 10;
-    }
-    shiftedNumber.push(shiftedDigit);
-  }
-
-  return shiftedNumber;
-}
-
-// ... kode sebelumnya ...
-
 function generateNumberSequenceSubtract() {
   const enteredNumber = Array.from(inputDigits)
     .map((digitInput) => digitInput.value)
@@ -194,7 +144,8 @@ function generateNumberSequenceSubtract() {
   // Clear previous results
   numberSequenceDisplaySubtract.innerHTML = "";
   nonRedNumbersSubtract.innerHTML = "";
-
+  // Hapus konten sebelumnya di dalam randomNumbersContainer
+  randomNumbersContainer.innerHTML = "";
   const redNumbers = [];
   const nonRedSubtract = [];
 
@@ -230,8 +181,40 @@ function generateNumberSequenceSubtract() {
     nonRedBox.textContent = digit;
     nonRedNumbersContainerSubtract.appendChild(nonRedBox);
   });
-  nonRedNumbersSubtract.appendChild(nonRedNumbersContainerSubtract);
+  nonRedNumbersSubtract.appendChild(nonRedNumbersContainerSubtract); // Setelah menghitung nonRedSubtract, tambahkan angkanya ke combinedNonRedNumbers.
+  nonRedSubtract.forEach((digit) => combinedNonRedNumbers.add(digit));
+
+  // ...
+
+  // Tampilkan combinedNonRedNumbers tanpa duplikat dalam tampilan HTML.
+  const uniqueNonRedNumbers = Array.from(combinedNonRedNumbers);
+  const combinedNonRedNumbersContainer = document.getElementById(
+    "combinedNonRedNumbers"
+  );
+  combinedNonRedNumbersContainer.innerHTML = uniqueNonRedNumbers
+    .map((digit) => {
+      return `<div class="number-box non-red">${digit}</div>`;
+    })
+    .join("");
 }
+
+function shiftNumber(number, shift, subtract) {
+  const shiftedNumber = [];
+  for (let i = 0; i < number.length; i++) {
+    const digit = parseInt(number[i]);
+    let shiftedDigit;
+    if (subtract) {
+      shiftedDigit = (digit - shift + 10) % 10;
+    } else {
+      shiftedDigit = (digit + shift) % 10;
+    }
+    shiftedNumber.push(shiftedDigit);
+  }
+
+  return shiftedNumber;
+}
+
+// ... kode sebelumnya ...
 
 function shiftNumberSubtract(number, shift) {
   const shiftedNumber = [];
@@ -242,3 +225,62 @@ function shiftNumberSubtract(number, shift) {
   }
   return shiftedNumber;
 }
+
+// Fungsi untuk memperbarui gabungan non-merah// Fungsi untuk memperbarui gabungan non-merah
+function updateCombinedNonRedNumbers() {
+  const combinedNonRedNumbersContainer = document.getElementById(
+    "combinedNonRedNumbers"
+  );
+  combinedNonRedNumbersContainer.innerHTML = "";
+
+  // Iterasi melalui angka non-merah yang unik dalam combinedNonRedNumbers
+  combinedNonRedNumbers.forEach((digit) => {
+    const digitBox = document.createElement("div");
+    digitBox.classList.add("number-box", "non-red");
+    digitBox.textContent = digit;
+    combinedNonRedNumbersContainer.appendChild(digitBox);
+  });
+}
+// Temukan tombol "Generate Random" berdasarkan ID-nya
+const generateRandomBtn = document.getElementById("generateRandomBtn");
+
+// Ambil referensi ke elemen tombol "Generate Random Numbers" dan kontainer untuk menampilkan angka acak
+const generateRandomNumbersBtn = document.getElementById(
+  "generateRandomNumbersBtn"
+);
+const randomNumbersContainer = document.getElementById("randomNumbers");
+
+// Fungsi untuk mengambil 4 angka acak dari array
+function getRandomNumbersFromArray(arr) {
+  const randomNumbers = [];
+
+  while (randomNumbers.length < 4) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const randomNumber = arr.splice(randomIndex, 1)[0];
+    randomNumbers.push(randomNumber);
+  }
+
+  return randomNumbers;
+}
+
+// Event listener untuk tombol "Generate Random Numbers"
+generateRandomNumbersBtn.addEventListener("click", () => {
+  // Salin isi dari combinedNonRedNumbers ke dalam array
+  const combinedNonRedNumbersArray = Array.from(combinedNonRedNumbers);
+
+  // Pastikan ada cukup angka untuk diambil
+  if (combinedNonRedNumbersArray.length >= 4) {
+    // Ambil 4 angka acak
+    const randomNumbers = getRandomNumbersFromArray(combinedNonRedNumbersArray);
+
+    // Tampilkan angka acak dalam kontainer
+    const randomNumbersDiv = document.createElement("div");
+    randomNumbersDiv.textContent = randomNumbers.join(" ");
+    randomNumbersContainer.appendChild(randomNumbersDiv);
+  } else {
+    // Pesan jika tidak ada cukup angka
+    const messageDiv = document.createElement("div");
+    messageDiv.textContent = "Tidak cukup angka untuk diambil.";
+    randomNumbersContainer.appendChild(messageDiv);
+  }
+});
